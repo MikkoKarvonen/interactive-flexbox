f2b1a3bd18b4c7aee0368a43287d724e3436784e
Cola Code-Monkey <j065cgs@kelam526-lrdxp-test.kela.root>
2018-02-07 10:41:46 +0200

    Add remaining controls

--- scripts.js
+++ scripts.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
     $('.flex-directions li button').click(function(e) {
-      document.getElementById("shapes").style.flexDirection = this.textContent;
-      addStyles();
+        document.getElementById("shapes").style.flexDirection = this.textContent;
+        addStyles();
+    });
+    $('.flex-wrap li button').click(function(e) {
+        document.getElementById("shapes").style.flexWrap = this.textContent;
+        addStyles();
+    });
+    $('.justify-content li button').click(function(e) {
+        document.getElementById("shapes").style.justifyContent = this.textContent;
+        addStyles();
+    });
+    $('.align-items li button').click(function(e) {
+        document.getElementById("shapes").style.alignItems = this.textContent;
+        addStyles();
     });
 });
